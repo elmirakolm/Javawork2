443bcb70741f3910491ed6a5728dc1ec89df896c
Elmira <elmira.kolmogorova@gmail.com>
2024-02-25 17:56:15 +0300

    Initial commit: Added .gitignore and project files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.print.attribute.SetOfIntegerSyntax;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 75;
+        int x = 20;
+        int miles = amount / x; // Рассчитываем количество начисленных миль
+        System.out.println("Начислено миль: " + miles);
+    }
+}
+
+
+
